8c9f8eed5b0a4d302e5a506120021d890ba16450
WangDake2333 <1078062469@qq.com>
2018-12-05 20:18:09 +0800

    Update TableInfo.sql
    Some grammatical errors have been corrected

--- TableInfo.sql
+++ TableInfo.sql
@@ -2,12 +2,12 @@
 create table teachers
 (teacher_id varchar(20) primary key,
 teacher_name varchar(20),
-teacher_password varchar(20)
+teacher_password varchar(20),
 sex varchar(2),
 institute varchar(20),
 address varchar(200),
 birthdate date,
-phone varchar2(11),
+phone varchar(11),
 e_mail varchar(50)
 );
 
@@ -16,12 +16,12 @@ e_mail varchar(50)
 create table deans
 (dean_id varchar(20) primary key,
 dean_name varchar(20),
-dean_password varchar(20)
+dean_password varchar(20),
 sex varchar(2),
 institute varchar(20),
 address varchar(200),
 birthdate date,
-phone varchar2(11),
+phone varchar(11),
 e_mail varchar(50)
 );
 
@@ -30,13 +30,13 @@ e_mail varchar(50)
 create table students
 (student_id varchar(20) primary key,      
 student_name varchar(20),
-class_name varchar(20)，
+class_name varchar(20),
 student_password varchar(20),
 sex varchar(2),
 institute varchar(20),
 address varchar(200),
 birthdate date,
-phone varchar2(11),
+phone varchar(11),
 e_mail varchar(50)
 );
 
@@ -65,16 +65,6 @@ correct_num int
 );
 
 
-/*出题表*/
-create table set_questions
-(paper_id varchar(10),
-question_id varchar(10),
-primary key (paper_id,question_id),
-foreign key (paper_id) references papers (paper_id),
-foreign key (question_id) references test (question_id)
-);
-
-
 /*试卷表*/
 create table papers
 (paper_id varchar(10) primary key,
@@ -84,6 +74,14 @@ stop_time datetime,
 foreign key (teacher_id) references teachers (teacher_id)
 );
 
+/*出题表*/
+create table set_questions
+(paper_id varchar(10),
+question_id varchar(10),
+primary key (paper_id,question_id),
+foreign key (paper_id) references papers (paper_id),
+foreign key (question_id) references questions (question_id)
+);
 
 /*学生-考试表*/
 create table sp
